61073c8cfbed17270223fbbab639e34eb9a06e55
betalphafai <edward-lk@hotmail.com>
2013-09-03 23:10:15 +0800

    Change the UI style

--- ProcExplorer/main.cpp
+++ ProcExplorer/main.cpp
@@ -1,10 +1,14 @@
 
-#include "ProcExplorerMain.h"
 #include <QApplication>
+#include <QStyleFactory>
+
+#include "ProcExplorerMain.h"
 
 int main(int argc, char *argv[])
 {
     QApplication _app(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("fusion"));
+
     ProcExplorerMain _win_main;
     _win_main.show();
     return _app.exec();
